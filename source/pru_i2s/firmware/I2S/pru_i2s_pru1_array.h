049eaaf44545584922bf25208027ab94be1ee425
rajul <r-bhambay@ti.com>
2025-04-17 16:22:31 +0530

    Am263x/AM261x: PRU-I2S
    - TDM functionality
    - I2S functionality
    
    Fixes: PINDSW-8487,9295
    
    Signed-off-by: Rajul Bhambay <r-bhambay@ti.com>

---
+++ source/pru_i2s/firmware/I2S/pru_i2s_pru1_array.h
@@ -0,0 +1,174 @@
+/*
+ *  Copyright (C) 2025 Texas Instruments Incorporated
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *    Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+const uint32_t pru_prupru_i2s1_image_0_0[] = {
+0x2effbf80,
+0x240200e7,
+0x241000e8,
+0x242000e9,
+0x24001af9,
+0x240002f7,
+0xe1001917,
+0x240001f7,
+0xe1011917,
+0x2400015a,
+0x2400017a,
+0x24001f1a,
+0x240019eb,
+0x24001bec,
+0x240008f7,
+0xf100178a,
+0x0b01eaea,
+0x10eaeaf1,
+0x0501f1f1,
+0x240014f7,
+0xf100378d,
+0x10ededef,
+0x2400203a,
+0x00f1edee,
+0x10eeeef0,
+0x0101f0f0,
+0xd105ff00,
+0xd109ff00,
+0xc909ff00,
+0xd109ff00,
+0xd105ff00,
+0xc905ff00,
+0x10e7fffc,
+0x10e8fff2,
+0x10e9fff3,
+0x60edee16,
+0x5100161e,
+0x511e160a,
+0x511f160e,
+0x511d1610,
+0x01011616,
+0x71013602,
+0x21004500,
+0x24000036,
+0x0b09fcfc,
+0x6856fc38,
+0x21004500,
+0x0b09fcfc,
+0x6856fc35,
+0x15015656,
+0x01011616,
+0x21004500,
+0x01013636,
+0x24000016,
+0x21004500,
+0x01011616,
+0x21004500,
+0x51017a03,
+0x10efefed,
+0x21003d00,
+0x10f0f0ed,
+0x00f1edee,
+0x15017a7a,
+0xe1000b7a,
+0x2400017b,
+0x21002800,
+0x01011616,
+0x21004500,
+0x01011616,
+0xd105ff00,
+0x69017b0b,
+0x2400007b,
+0xf1000c3b,
+0x69007a04,
+0xd1013b17,
+0x1f003b3b,
+0x21004f00,
+0xd1003b14,
+0x1f013b3b,
+0xe1000c3b,
+0x2400269f,
+0x21005200,
+0x51203a08,
+0x0b0cf2f2,
+0x0b0df3f3,
+0x083af2f2,
+0x083af3f3,
+0x12f2f4f4,
+0x12f3f5f5,
+0x51003a03,
+0x05013a3a,
+0x21001f00,
+0xe1006d94,
+0x0108eded,
+0x2eff8792,
+0x24001f3a,
+0x21001f00,
+0x1f005b5b,
+0x21006600,
+0x1f015b5b,
+0x21006600,
+0x1f025b5b,
+0x24001cf9,
+0xe100195b,
+0x2400279f,
+0x21000000};
+
+const uint8_t pru_prupru_i2s1_image_0_1[] = {
+0x00,
+0x02,
+0x30,
+0x20,
+0x00,
+0x02,
+0x01,
+0x00,
+0x00,
+0x01,
+0x15,
+0x16,
+0x17,
+0x00,
+0x01,
+0x04,
+0x05,
+0x06,
+0x02,
+0x03,
+0x00,
+0x03,
+0x01,
+0x00,
+0x00,
+0x00,
+0x02,
+0x01,
+0x00,
+0x00,
+0x00,
+0x00};
+
