c03b5016a2347862c8a7aea1d91269a408778aef
Thomas Leyrer <t-leyrer@ti.com>
2025-05-27 16:30:16 +0530

    am243x: Add LCD interface example
    Signed-off-by: Thomas Leyrer <t-leyrer@ti.com>
    Signed-off-by: Pratheesh Gangadhar <pratheesh@ti.com>

---
+++ examples/LCD_interface/mcuplus/LCD_interface.c
@@ -0,0 +1,122 @@
+/*
+ *  Copyright (C) 2024-2025 Texas Instruments Incorporated
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *    Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdio.h>
+#include <kernel/dpl/DebugP.h>
+#include "ti_drivers_config.h"
+#include "ti_drivers_open_close.h"
+#include "ti_board_open_close.h"
+#include <drivers/pruicss.h>
+
+#include <pru0_load_bin.h>
+#include <pru1_load_bin.h>
+
+/* register configuration defines */
+#define KICK_LOCK_VAL                           (0x00000000U)
+#define KICK0_UNLOCK_VAL                        (0x68EF3490U)
+#define KICK1_UNLOCK_VAL                        (0xD172BC5AU)
+/* PADMMR config register - partition 0 */
+#define KICK0_UNLOCK_P0_REG                     (0x000f1008U)
+#define KICK1_UNLOCK_P0_REG                     (0x000f100CU)
+/* PADMMR config register - partition 1 */
+#define KICK0_UNLOCK_P1_REG                     (0x000f5008U)
+#define KICK1_UNLOCK_P1_REG                     (0x000f500CU)
+/* padconfig for PRG0_PWM2_A2  BP.40 */
+#define PAD_PRG0_PWM2_A2_ADDR                   (0x000F4048U)
+#define PAD_GPO_MODE9                           (0x00010009U)
+/* ICSS_G PWM2 trip config - we use PWM2 init state as GPO */
+#define ICSSG_PWM2_TRIP_CFG_REG                 (0x30026138U)
+#define PWM2_TRIP_CLEAR                         (0x0003ff00U)
+#define PWM2_TRIP_RESET                         (0x0004ff00U)
+
+/*
+ *  This is an example project to show R5F
+ *  loading PRU firmware.
+ */
+
+/** \brief Global Structure pointer holding PRUSS1 memory Map. */
+
+PRUICSS_Handle gPruIcss0Handle;
+
+
+void LCD_interface_main(void *args)
+{
+     Drivers_open(); // check return status
+
+     int status;
+     status = Board_driversOpen();
+     DebugP_assert(SystemP_SUCCESS == status);
+
+     gPruIcss0Handle = PRUICSS_open(CONFIG_PRU_ICSS0);
+
+     status = PRUICSS_initMemory(gPruIcss0Handle, PRUICSS_DATARAM(PRUICSS_PRU0));
+     DebugP_assert(status != 0);
+
+     status = PRUICSS_initMemory(gPruIcss0Handle, PRUICSS_DATARAM(PRUICSS_PRU1));
+     DebugP_assert(status != 0);
+
+     /* ---------- pin-mux configuration --------------------- */
+
+
+     // unlock PADMMR config register
+     // partition 0
+     HW_WR_REG32(KICK0_UNLOCK_P0_REG, KICK0_UNLOCK_VAL);
+     HW_WR_REG32(KICK0_UNLOCK_P0_REG, KICK1_UNLOCK_VAL);
+
+     // unlock PADMMR config register
+     // partition 1
+     HW_WR_REG32(KICK0_UNLOCK_P1_REG, KICK0_UNLOCK_VAL);
+     HW_WR_REG32(KICK0_UNLOCK_P1_REG, KICK1_UNLOCK_VAL);
+     /* padconfig for PRG0_PWM2_A2  BP.40 */
+     HW_WR_REG32(PAD_PRG0_PWM2_A2_ADDR, PAD_GPO_MODE9);
+
+     /* ---------- PWM in init mode  --------------------- */
+
+     // force PWM2 trip mode to move to init state
+     HW_WR_REG32(ICSSG_PWM2_TRIP_CFG_REG, PWM2_TRIP_CLEAR);
+     HW_WR_REG32(ICSSG_PWM2_TRIP_CFG_REG, PWM2_TRIP_RESET);
+
+
+     status = PRUICSS_loadFirmware(gPruIcss0Handle, PRUICSS_PRU1, PRU1Firmware_0, sizeof(PRU1Firmware_0));
+     DebugP_assert(SystemP_SUCCESS == status);
+     status = PRUICSS_loadFirmware(gPruIcss0Handle, PRUICSS_PRU0, PRU0Firmware_0, sizeof(PRU0Firmware_0));
+     DebugP_assert(SystemP_SUCCESS == status);
+
+     while (1)
+     {
+      /* ARM is in idle loop, PRUs perform framebuffer output */
+        ClockP_usleep(1);
+     }
+
+     Board_driversClose();
+     Drivers_close();
+}
