4ddbd2e12972aa561316320605c4183a30cc0a04
Aravind-Padma-Kumar <a-kumar13@ti.com>
2025-08-27 12:49:39 +0200

    Added Logic Scope Example

---
+++ examples/logic_scope/README.md
@@ -0,0 +1,60 @@
+# 8-channel Logic Scope
+
+The project realizes a simple logic scope that uses two cores, PRU0 and RTU0, to continuously sample a configured GPI Pin. The sampling frequency is restricted only by the clock frequency of PRU and for AM243 we could configure the clock upto 333Mhz. This gives us a maximum sampling rate of 333Mhz. The number of samples that need to be captured can be configured in multiples of 100 and the maximum number of samples is only restricted by the size of the configured storage memory. For MSRAM, this is around 2MB of sample size. The Logic-scope reads 1 Byte of data every clock cycle which contains logic data from 8 input channels.  The trigger for sampling in current implementation is set to a low to high in the 8th channel (PRU0.GPI7).
+![alt text](images/DCLS_overview.png)
+
+## Overview
+
+PRU cores can directly read certain input pins using their R30 registers. Since R30 has real-time information on the logic levels of these pins, a zero-overhead loop and indirect register addressing could be used to continuously transfer the samples to other registers, bytewise. The number of samples we can transfer continously in this case, is limited by the number of available registers in a single core. Once we are out of registers, with a single core, we have no other choice but to pause sampling to tranfer the collected samples to memory. 
+
+The above mentioned restriction can be surpassed by making use of an extra core which can continue sampling after the first core fills up it's registers. This way, one core will always continue sampling while the other core stores the data that it sampled. The cores are selected as PRU and RTU since they will have access to the same input pins simultaneously. A conceptual timing diagram of the same is shown below:
+
+![alt text](images/DCLS_timing.png)
+
+The cores have to be synchronized once before they wait for the trigger together. This is performed with the help of IPC SPAD between PRU and RTU. 
+
+## Configuration
+
+1. DCLS_config_macros.inc contains all the configurations for DCLS. It is imortant to note that required configurations are to be done in this file for both cores
+
+2. 8 channels are sampled simultaneously. Set the channel Byte to be sampled as 0, 1, or 2. For GPI0-GPI7, channel Byte is 0. Note that depending on the availability of pins, changing the Byte number will reduce the number of usable channels. 
+    ```
+    DCLS_CH_BYTE .set 0  
+    ```
+3. Set the trace memory. For utilizing the full 2MB trace, set the memory location to MS-RAM as follows :
+    ```
+    DCLS_SMP_MEM_GLB  .set 0x70000000
+    ```
+4. Samples are traced in multiples of 100. Each sample is 1 Bytes. It is important to take care that the size does not exceed the limits of configured memory when changing this value. MS-RAM for example, 
+
+    ```math
+    Span : 0x70000000 – 0x701FFFFF
+    ```
+    ```math
+    Size : 0x1FFFFF (2MB)
+    ```
+    ```math
+    max\_sample\_sets = ((0x1FFFFF) / 100) = 20971
+    ```
+    
+    Now the sample size could be set to a value below max, like as shown below in default configuration:
+    ```
+    DCLS_SMP_CNT    .set 20900 
+    ```
+5. Default trigger is set to channel 7 and is looking for a low to high trigger signal. For setting the trigger on another channel, change the value of the following parameter to any channel number from 0-7:
+    ```
+    DCLS_TRIG_CH    .set 7
+
+    ```
+
+## How to Run
+1. Connect the input signal to be traced to one of the channels in logic scope. If multiple signals are to be traced, connect all inputs to the configured channels.
+2. Build the PRU0 and RTU0 projects in "fft/examples/logic_scope" and load the projects to corresponding cores.
+3. Run the RTU0 project first, which is important for core synchronization. After the RTU0 project starts running, run the PRU0 project.
+
+    Note :After both cores are up and running, the logic scope will actively sample for a low to high trigger on the configured channel. Once it detects the trigger, the configured number or samples will be collected in the configured memory. By default, a ~2MB will be stored in MS-RAM. 
+4. To view the data from collected 8-channel samples, connect to an ARM core and open memory browser. Save the binary trace that starts from configured memory. For default configuration of logic scope, using CCS, we can save this trace as 'TI Raw Data'. The parameters will look as in the following image:
+    ![DCLS_trace_collection](images/DCLS_trace_collection.png)
+5. The collected binary trace can be viewed in  a tool like 'PulseView'. In PulseView, import the data as raw binary, set the number of channels as 8 and the sampling frequency(333Mhz by default).
+6. The sampled signals over 8 channels can now be viewed as shown below(only one channel is connected) :
+    ![DCLS_pulse_view_op](images/DCLS_pulse_view_out.png)
---
+++ examples/logic_scope/firmware/am243x-lp/icss_g0_pru0_fw/ti-pru-cgt/logic_scope.asm
@@ -0,0 +1,219 @@
+; Copyright (C) 2024-2025 Texas Instruments Incorporated - http://www.ti.com/
+;
+; Redistribution and use in source and binary forms, with or without
+; modification, are permitted provided that the following conditions
+; are met:
+;
+;        * Redistributions of source code must retain the above copyright
+;          notice, this list of conditions and the following disclaimer.
+;
+;        * Redistributions in binary form must reproduce the above copyright
+;          notice, this list of conditions and the following disclaimer in the
+;          documentation and/or other materials provided with the
+;          distribution.
+;
+;        * Neither the name of Texas Instruments Incorporated nor the names of
+;          its contributors may be used to endorse or promote products derived
+;          from this software without specific prior written permission.
+;
+; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+; "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+; LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+; A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+; OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+; SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+; LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+; DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+; THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+; OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+	.sect	".text:logic_scope"
+	.clink
+	.global	||FN_LOGIC_SCOPE_DUAL_CORE||
+
+;==========================================================================;
+;                             Include Files                                ;
+;==========================================================================;
+	.include "pru_io/firmware/common/icss_xfer_defines.inc"
+	.include "dcls_config_macros.inc"
+;*******************************************
+;   Function : FN_LOGIC_SCOPE_DUAL_CORE
+;
+;   Description:
+;   			Collects continuous samples over two cores using R30 registers
+;	Pre-requisites:
+;				1. Function should run on PRU0 and RTU0
+;				2. Function in RTU0 should start before the one in PRU0
+;				3. configuration constants are set to valid values in DCLS_config_macros.inc
+;   Pseudocode:
+;			//configure constants as set in DCLS_config_macros.inc
+;				Rx --> R2.b0	//point to first byte of R2
+;			//synchronization of both cores
+;			//wait for low to high trigger
+;			#ifdef PRU0
+;			{
+;			START_SAMPLING:
+;				WHILE (100)	//collect 100 samples
+;					{
+;						MOV *Rx.b0++, R30.bx	// bx configured as CHANNEL BYTE
+;						// Register pointer updated in the last instruction
+;			 		}
+;				WRITE (&R2, MEM_POINTER, 100)	// Save 100 samples collected that starts from R2
+;				MEM_POINTER += 200	// update pointer for next cycle
+;				Rx --> R2.b0	//point back to first byte of R2
+;				WAIT(X)	// wait precisely the remaining time till other core finishes sampling
+;				CORE_SAMPLE_COUNT_1--;	//update sample count
+;				if (CORE_SAMPLE_COUNT_1 != 0) GOTO START_SAMPLING
+;			}
+;			#ifdef RTU0
+;			{
+;			CYCLE_CORE_1:
+;				WAIT(Y) //wait for other core to finish sampling
+;				if (CORE_SAMPLE_COUNT_2 == 0) GOTO END_SAMPLING
+;				WHILE (100)	//collect 100 samples
+;					{
+;						MOV *Rx.b0++, R30.bx	// bx configured as CHANNEL BYTE
+;						// Register pointer updated in the last instruction
+;			 		}
+;				WRITE (&R2, MEM_POINTER, 100)	// Save 100 samples collected that starts from R2
+;				MEM_POINTER += 200	// update pointer for next cycle
+;				Rx --> R2.b0	//point back to first byte of R2
+;				CORE_SAMPLE_COUNT_2--;	//update sample count
+;				GOTO CYCLE_CORE_1
+;			END_SAMPLING:
+;			}
+;	Cycle budget: Config + Synchronization Core0(PRU0) : 15 or 16 cycles
+;				 				  		   Core1(RTU0) : (15 or 16) cycles plus PRU0 start delay
+
+;				  Logic Scope : 1+(100*(N+1)) cycles, N: number of samples in multiples of 100 (n=5 => 500 samples)
+;
+;*******************************************
+||FN_LOGIC_SCOPE_DUAL_CORE||:
+	;***********************
+	;**	Pre-configuration **
+	;***********************
+	ldi  r0.b0, 100				; fixed 100 samples sampled by one core at a time
+	ldi  r1.b0, 8				; sampling starts from r2
+	ldi32 r29, DCLS_SMP_MEM_GLB	; set sample storage mem address
+	.if (DCLS_CORE == 0) 		; PRU0
+	ldi  r28, MEM_OFFSET_CORE0 	; set memory offset start
+	ldi  r27, DCLS_SMP_CNT_C0 	; set initial sample size
+	.elseif (DCLS_CORE == 1) 	; RTU0
+	ldi  r28, MEM_OFFSET_CORE1 	; set memory offset start
+	ldi  r27, DCLS_SMP_CNT_C1 	; set initial sample size
+	.endif
+
+	;*********************
+	;**	Synchronization **
+	;*********************
+	; PRU - RTU sync synchronization before waiting for trigger
+	; Uses IPC SPAD r2.b0 and r2.b1
+	; (!!only works when RTU is started before PRU!!)
+	.if (DCLS_CORE == 0) ; PRU0
+	;load sync bit
+	ldi r2.b0, 1
+	ldi r2.b1, 0
+	; set trigger and poll for response
+	xout	PRU_RTU_SPAD_XID, &r2.b0, 1
+    nop
+    nop
+    nop
+    xin		PRU_RTU_SPAD_XID, &r2.b1, 1
+    qbbs	DCLS_SYNC_CASE_1, r2.b1, 0
+    xin		PRU_RTU_SPAD_XID, &r2.b1, 1
+    qbbs	DCLS_SYNC_CASE_2, r2.b1, 0
+    ; none occurred
+    halt
+DCLS_SYNC_CASE_1: ; PRU1 sees trigger early
+    nop
+    qba		DCLS_SYNC_CMP	   ; sync complete
+DCLS_SYNC_CASE_2: ; PRU1 sees trigger late
+    ; send one cycle earlier as it took
+    ; PRU1 longer to detect.
+    qba	DCLS_SYNC_CMP	   ; sync complete
+
+	.elseif (DCLS_CORE == 1) ; RTU0
+; requires to RTU0 to start before PRU0
+	; clear SPAD r2.b0 and r2.b1
+	ldi r2.w0, 0
+	xout   PRU_RTU_SPAD_XID, &r2.b0, 2
+	;load sync bit
+	ldi r2.b1, 1
+DCLS_SYNC_WAIT_FOR_TRIGGER:
+    xin    PRU_RTU_SPAD_XID, &r2.b0, 1
+    qbne   DCLS_SYNC_WAIT_FOR_TRIGGER, r2.b0, 1
+    xout   PRU_RTU_SPAD_XID, &r2.b1, 1
+    nop
+    nop
+    nop
+    qba	DCLS_SYNC_CMP	   ; sync complete
+	.endif
+
+DCLS_SYNC_CMP:
+;*** End Sync ***
+
+	;******************
+	;**	Logic Scope  **
+	;******************
+	; both cores wait for a low to high trigger
+	wbc  r31, DCLS_TRIG_CH
+	wbc  r31, DCLS_TRIG_CH	; re-check to filter single cycle glitches
+	wbs  r31, DCLS_TRIG_CH
+
+	;**************
+	;**	 Core 0  **
+	;**************
+	.if (DCLS_CORE == 0)
+DCLS_START_SAMPLING:
+	; core 0 starts sampling Byte M() after trigger
+	loop DCLS_SMP_LOOP_0, r0.b0
+	m_sample_channel_byte
+DCLS_SMP_LOOP_0:
+	; transfer 100 samples to memory
+	sbbo &r2, r29, r28, 100
+	; update offset for storage
+	add r28, r28, 200
+	; point back to r2 for next cycle
+	ldi  r1.b0, 8
+	; wait till the other core finishes sampling
+	m_nopx DCLS_SMP_WT_TIME
+	; update the remaining sample count
+	sub r27, r27, 1
+	; check sample counter and start a new cycle of samples if needed
+	qble DCLS_START_SAMPLING, r27, 1
+
+	;**************
+	;**	 Core 1  **
+	;**************
+	; (first loop unrolled to manage wait times)
+	.elseif (DCLS_CORE == 1)
+	; wait till first core finishes sampling
+	m_nopx (100 - DCLS_SMP_WT_TIME) ; additional wait time for the first cycle
+DCLS_CYCLE_CORE_1:
+	m_nopx (DCLS_SMP_WT_TIME-1)	; wait time for normal cycles
+	; end sampling if no count remains
+	qbge DCLS_END_SAMPLING, r27, 0
+	; core 1 starts sampling
+	loop DCLS_SMP_LOOP_1, r0.b0
+	m_sample_channel_byte
+DCLS_SMP_LOOP_1:
+	; transfer 100 samples to memory
+	sbbo &r2, r29, r28, 100
+	; update offset for storage
+	add r28, r28, 200
+	; point back to r2 for next cycle
+	ldi  r1.b0, 8
+	; update the sample count
+	sub r27, r27, 1
+	; repeat cycle
+	qba DCLS_CYCLE_CORE_1
+DCLS_END_SAMPLING:
+	.endif
+	halt
+
+;*****************************************************************************
+;                                     END
+;*****************************************************************************
