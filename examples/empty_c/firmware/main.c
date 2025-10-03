8f21abb6efd51362241d92c3f6ddf6120cac6e91
Nick Saulnier <nsaulnier@ti.com>
2025-10-03 14:40:41 -0500

    examples/empty_c: Add empty C example
    empty_c is the starting point for C and mixed C & assembly development.
    
    Signed-off-by: Nick Saulnier <nsaulnier@ti.com>

---
+++ examples/empty_c/firmware/main.c
@@ -0,0 +1,11 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (C) 2021 Texas Instruments Incorporated - http://www.ti.com/
+ */
+
+#include <stdint.h>
+
+int main(void)
+{
+	__halt();
+}
