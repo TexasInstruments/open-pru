04ec679f2bf74258f007828f05f6b1aee8ae744e
Nick Saulnier <nsaulnier@ti.com>
2025-10-04 19:54:29 -0500

    academy: remove unneeded MCU+ SDK dependencies
    Some projects included files in the MCU+ SDK repo, even though the
    files were never referenced. Remove external dependencies for all projects
    where the external dependency is not currently needed.
    
    While modifying these files, I also switched to the shortened license format
    to reduce scrolling when accessing the files in the future.
    
    Signed-off-by: Nick Saulnier <nsaulnier@ti.com>

--- academy/crc/crc/firmware/main.asm
+++ academy/crc/crc/firmware/main.asm
@@ -1,32 +1,5 @@
+; SPDX-License-Identifier: BSD-3-Clause
 ; Copyright (C) 2024-2025 Texas Instruments Incorporated - http://www.ti.com/
-;
-; Redistribution and use in source and binary forms, with or without
-; modification, are permitted provided that the following conditions
-; are met:
-;
-; Redistributions of source code must retain the above copyright
-; notice, this list of conditions and the following disclaimer.
-;
-; Redistributions in binary form must reproduce the above copyright
-; notice, this list of conditions and the following disclaimer in the
-; documentation and/or other materials provided with the
-; distribution.
-;
-; Neither the name of Texas Instruments Incorporated nor the names of
-; its contributors may be used to endorse or promote products derived
-; from this software without specific prior written permission.
-;
-; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-; "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-; LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-; A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-; OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-; SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-; LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-; DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-; THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-; OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 ;***************************************************************************************
 ;   File:     main.asm
@@ -53,16 +26,14 @@
     .global     main
     .sect       ".text"
 
-;************************************* includes *************************************
-; icss_constant_defines.inc: Defines symbols corresponding to Constant Table Entries
-    .include "icss_constant_defines.inc"
-
+; Definitions
     .asg    0x1,           CRC_ID          ; CRC_ID
     .asg    0x00000000,    CRC16_MODE      ; CRC 16 mode (write 4 bytes at a time)
     .asg    0x00000001,    CRC32_MODE      ; CRC 32 mode (write 4 bytes at a time)
     .asg    R10,           TEMP_CRC16      ; Temporary register for CRC16
     .asg    R11,           TEMP_CRC32      ; Temporary register for CRC32
     .asg    0x3412,        DATA16          ; 0x1234 written in little-endian format
+
 ;********
 ;* MAIN *
 ;********
