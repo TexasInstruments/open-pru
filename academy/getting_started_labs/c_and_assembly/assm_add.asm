2ffc75a8fdee098f9ee8075b35fb02b9542ca77d
Nick Saulnier <nsaulnier@ti.com>
2025-10-09 09:08:55 -0500

    academy: Add Getting Started Labs
    Signed-off-by: Nick Saulnier <nsaulnier@ti.com>

---
+++ academy/getting_started_labs/c_and_assembly/assm_add.asm
@@ -0,0 +1,36 @@
+; SPDX-License-Identifier: BSD-3-Clause
+; Copyright (C) 2018-2025 Texas Instruments Incorporated - http://www.ti.com/
+
+;******************************************************************************
+; Build Configuration
+;******************************************************************************
+
+; Required for building .out with assembly file
+    .retain
+    .retainrefs
+
+; .sect ".text:assm_add" places all code below the .sect directive into the
+; .text section, grouped into a subsection named "assm_add"
+    .sect	".text:assm_add"
+    .clink
+    .global	assm_add
+
+;*****************************************************************************
+; uint32_t assm_add(uint32_t arg1, uint32_t arg2);
+;*****************************************************************************
+assm_add:
+
+;-----------------------------------------------------------------------------
+;   Function input arguments are stored in R14-R29.
+;   So the 32 bit value in arg1 is stored in R14, and the 32 bit value in
+;   arg2 is stored in R15. The return value is stored in R14.
+;
+;   For more details about how function arguments are stored in registers,
+;   reference the document "PRU Optimizing C/C+ Compiler User's Guide",
+;   section "Function Structure and Calling Conventions"
+;-----------------------------------------------------------------------------
+
+    ; TODO: add arg1 and arg2. Store the sum in the return register
+
+    ; return from function assm_add
+    JMP		r3.w2
---
+++ academy/getting_started_labs/c_code/solution/firmware/main.c
@@ -0,0 +1,44 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (C) 2018-2025 Texas Instruments Incorporated - http://www.ti.com/
+ */
+
+#include <stdint.h>
+
+/* TODO: define c */
+/* a, b, and c are stored in a defined location in PRU memory */
+#define a  (*((volatile unsigned int *)0x110))
+#define b  (*((volatile unsigned int *)0x114))
+#define c  (*((volatile unsigned int *)0x118))
+
+/*
+ * main.c
+ */
+void main(void)
+{
+	/* TODO: define y & z */
+	/* The compiler decides where to store x, y, and z */
+	uint32_t x = 1;
+	uint32_t y = 2;
+	uint32_t z = 0;
+
+	a = 1;
+	b = 2;
+
+	while(1) {
+		/*
+		 * TODO: store the sum of x and y in z
+		 */
+		z = x + y;
+
+		/*
+		 * TODO: store the sum of the numbers at memory locations a and
+		 * b in memory location c
+		 */
+		c = a + b;
+	}
+
+	/* This program will not reach __halt because of the while loop */
+	__halt();
+}
+
